161914ce67ca641a55bc585a19f0b3352283e3c7
Richie Flores <rfloresc@icloud.com>
2024-12-30 16:12:32 -0800

    Initial Commit

---
+++ Supscription/SupscriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupscriptionApp.swift
+//  Supscription
+//
+//  Created by Richie Flores on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SupscriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
