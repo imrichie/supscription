800e46a3fc03a28f0f84b9dded51693125f4ea13
Richie Flores <rfloresc@icloud.com>
2025-03-09 00:39:40 -0800

    Add descriptive comments

--- Supscription/Views/ContentView.swift
+++ Supscription/Views/ContentView.swift
@@ -9,26 +9,31 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    // fetches all subscriptions from SwiftData
     @Query var subscriptions: [Subscription]
     
+    // state management
     @State private var selectedCategory: String? = "All Subscriptions"
     @State private var selectedSubscription: Subscription? = nil
     @State private var searchText: String = ""
     @State private var isAddingSubscription: Bool = false
     
+    // computes a list of unique categories and ensures All Categories is always first
     var uniqueCategories: [String] {
         let categories = Set(subscriptions.map { $0.category}).sorted()
         return ["All Subscriptions"] + categories
     }
     
-    var filteredSubscriptions: [Subscription] { // Computed property
+    // filters subscriptions based on the currently selected category
+    var filteredSubscriptions: [Subscription] {
+        // if the selected category is already All Subscriptions then just return the fetched array
+        // otherwise return a filtered array based on the selected category
         if selectedCategory == "All Subscriptions" {
             return subscriptions
         } else {
             return subscriptions.filter { $0.category == selectedCategory ?? ""}
         }
     }
-
     
     var body: some View {
         NavigationSplitView {
