d474532ccf812ff2def4c372e79924029e362a40
Richie Flores <rfloresc@icloud.com>
2025-04-05 14:29:08 -0700

    Polished the welcome view onboarding sheet

---
+++ Supscription/Views/EmptyStates/WelcomeSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeSheetView.swift
+//  Supscription
+//
+//  Created by Ricardo Flores on 4/4/25.
+//
+
+import SwiftUI
+
+struct WelcomeSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeSheetView()
+}
